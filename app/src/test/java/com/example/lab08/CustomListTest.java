70fc040a8802fdc50a221f36bd0f1f21abd93b45
fannnen821 <chiaen@ualberta.ca>
2025-10-29 20:15:36 -0600

    CustomListTest.java updated

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,14 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    void testHasCity() {
+        CustomList list = new CustomList(null, null);
+        City city = new City("Edmonton", "Alberta");
+        list.addCity(city);
 
+        assertTrue(list.hasCity(city));
+
+        City nonExisting = new City("Toronto", "Ontario");
+        assertFalse(list.hasCity(nonExisting));
+    }
 }
